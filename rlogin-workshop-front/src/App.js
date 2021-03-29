27bef8e7dc7aa3225ce30b893e793795cb63c2e6
Ilan Olkies <ilanolkies@outlook.com>
2021-03-29 15:36:56 -0300

    Add back end

--- rlogin-workshop-front/src/App.js
+++ rlogin-workshop-front/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import axios from 'axios'
 import RLogin, { RLoginButton } from '@rsksmart/rlogin'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import logo from './rif-id-social.png'
@@ -16,22 +17,45 @@ const rLogin = new RLogin({
       }
     }
   },
+  backendUrl: 'http://localhost:3001',
   supportedChains: [31] // enable rsk testnet network
 })
 
 
 function App() {
-  const [provider, setProvider] = useState()
+  const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState('')
 
-    // display pop up
+  const [to, setTo] = useState('')
+  const [value, setValue] = useState('')
+  const [txHash, setTxHash] = useState('')
+
+  const [notProtectedResponse, setNotProtectedResponse] = useState('')
+  const [protectedResponse, setProtectedResponse] = useState('')
+
   const connect = () => rLogin.connect()
-    .then(response => { // the provider is used to operate with user's wallet
-      setProvider(response.provider)
+    .then(({ provider }) => { // the provider is used to operate with user's wallet
+      setProvider(provider)
 
       // request user's account
-      response.provider.request({ method: 'eth_accounts' }).then(([account]) => setAccount(account))
+      provider.request({ method: 'eth_accounts' }).then(([account]) => setAccount(account))
     })
+
+  // send a transaction with user's wallet
+  const send = () => provider.request({ method: 'eth_sendTransaction', params: [
+    { from: account, to, value }
+  ]}).then(setTxHash)
+
+  // interact with back-end
+  const handleAxiosResponse = (query, handler) => query
+    .then(({ data }) => handler(data))
+    .catch(({ message }) => handler(message))
+
+  const getNotProtected = () => handleAxiosResponse(axios.get('http://localhost:3001/not-protected'), setNotProtectedResponse)
+
+  const getProtected = () => handleAxiosResponse(axios.get('http://localhost:3001/protected', {
+    headers: { 'Authorization': `DIDAuth ${localStorage.getItem('RLOGIN_ACCESS_TOKEN')}` }
+  }), setProtectedResponse)
 
   return (
     <div className="App">
@@ -42,6 +66,22 @@ function App() {
       <div>
         <RLoginButton onClick={connect}>Connect wallet</RLoginButton>
         <p>wallet address: {account}</p>
+      </div>
+      <hr />
+      <div>
+        <input type="text" value={to} onChange={e => setTo(e.target.value)} placeholder="to" />
+        <input type="number" value={value} onChange={e => setValue(e.target.value)} placeholder="value" />
+        <button onClick={send}>Send</button>
+        <p>tx: {txHash}</p>
+      </div>
+      <hr />
+      <div>
+        <button onClick={getNotProtected}>not protected endpoint</button>
+        <p>Response: {notProtectedResponse}</p>
+      </div>
+      <div>
+        <button onClick={getProtected}>protected endpoint</button>
+        <p>Response: {protectedResponse}</p>
       </div>
     </div>
   );
